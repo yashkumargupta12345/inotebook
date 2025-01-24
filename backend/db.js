a45c4c991e0fc9dcd4ea082c00b5b0c206268ff0
yashkumargupta12345 <guptayashyg45227@gmail.com>
2025-01-25 03:08:29 +0530

    Added the express-validator to check the schema validity

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const MONGO_URI= "mongodb://localhost:27017/";
+const MONGO_URI= "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
 
 const connectToMongo = () => {
     mongoose.connect(MONGO_URI).then(
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const { body, validationResult } = require('express-validator');
 
 router.get("/", (req, res) => {
     const obj = {
@@ -10,10 +11,23 @@ router.get("/", (req, res) => {
     res.json(obj);
 });
 
-router.post("/", (req, res) => {
-        const user = new User(req.body);
-        user.save();
-        res.send("User created");
+router.post("/", [
+    body('name', "Enter a valid Name").isLength({ min: 3 }),
+    body('email', "Enter a valid Email").isEmail(),
+    body('password', "Password must be atleast 5 characters").isLength({ min: 5 })
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then(user => res.json(user)).catch(err => {
+        console.error(err.message);
+        res.status(500).send("Some error occured");
+    });
 });
 
 
